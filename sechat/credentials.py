ee6da3892dd64ee867bded7668e6d4f181a3b213
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2026-04-20 17:49:04 +0100

    Fix conditional check for META_STACK_EXCHANGE

--- sechat/credentials.py
+++ sechat/credentials.py
@@ -98,7 +98,7 @@ class Credentials:
             "https://stackoverflow.com"
             if server == Server.STACK_OVERFLOW
             else "https://meta.stackexchange.com"
-            if server == META_STACK_EXCHANGE
+            if server == Server.META_STACK_EXCHANGE
             else "https://codegolf.stackexchange.com"
         )
         cookie_root = (
